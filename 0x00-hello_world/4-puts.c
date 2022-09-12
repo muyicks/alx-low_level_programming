3690e06d6e279f334924671a0beb57aa4b3a6343
Abel <muyicks@gmail.com>
2022-09-12 09:27:49 -0700

    hmm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,10 @@
  * retun 0 if suceesful
  */
 int main(void)
-
+/**                                                                                                                                     
+ * this is where the puzzle comment i
+ * retun 0 if suceesf
+ */    
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
