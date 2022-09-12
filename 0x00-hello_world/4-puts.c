9d29e8dfd725be896097356d2e7ff8ec9c701925
Abel <muyicks@gmail.com>
2022-09-11 20:38:00 -0700

    another one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *
- this function is  a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line usinf puts
+ 
+ this function is  a C program that prints exactly
  */
 int main(void)
 {
