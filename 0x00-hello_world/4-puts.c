201caba9ec99593dc8ff9bd9812c21792f86c39d
Abel <muyicks@gmail.com>
2022-09-11 20:35:25 -0700

    hope no error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *
+ this function is  a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line usinf puts
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
