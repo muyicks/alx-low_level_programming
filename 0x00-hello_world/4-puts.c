f1bbd7ce2eae5945143f06105203d78c76cfd5c5
Abel <muyicks@gmail.com>
2022-09-12 17:05:32 -0700

    pls work ooh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include <studio.h>
+
 /**
- * this is where the puzzle comment is
- * retun 0 if suceesful
- */
+ * *main - Entry point
+ * *
+ * *Return: Always 0
+ * */
 int main(void)
-/**                                                                                                                                     
- * this is where the puzzle comment i
- * retun 0 if suceesf
- */    
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
