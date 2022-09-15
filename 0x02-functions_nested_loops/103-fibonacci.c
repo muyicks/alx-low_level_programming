965b285f6f769fe6f6bf6ddd43d3355ea62c5cba
Abel <muyicks@gmail.com>
2022-09-14 20:49:51 -0700

    wow

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- * *main - prints sum of even fibonacci
- *
- * *suit elements
- *
- * *fibonacci suit numbers
- *
- * *Return: return 0
- *
+ * main - prints sum of even fibonacci
+ * suit elements
+ * fibonacci suit numbers
+ * Return: return 0
 */
 
 int main(void)
