e862d4b623266a84311d839948ef081b7e59051d
Abel <muyicks@gmail.com>
2022-09-22 21:28:11 -0700

    i hope this magic works

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	  int n;
+	  int a[5];
+	  int *p;
+
+	  a[2] = 1024;
+	  p = &n;
+	  /*
+	   * write your line of code here...
+	   * Remember:
+	   * u are not allowed to use a
+	   */
+	 printf("a[2] = %d\n", a[2]);
+	return (0);
+}	
